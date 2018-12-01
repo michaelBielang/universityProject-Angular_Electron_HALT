c6b5f4df5dd526e971587010f912e7aca0c14bc7
nrg <michael.bielang@hs-augsburg.de>
2018-12-01 15:55:53 +0100

    working add and present function for user. Added productive db fix path

--- src/api/datastorage/datastorage.js
+++ src/api/datastorage/datastorage.js
@@ -9,19 +9,19 @@
 
 'use strict'
 const createTableStatements = {
-  studySubject: 'CREATE TABLE study_subject(pk_study_id INT PRIMARY KEY AUTOINCREMENT NOT NULL, study_name VARCHAR(255) NOT NULL, FOREIGN KEY (fK_faculty_name) REFERENCES faculty(pk_faculty_name))',
-  user: 'CREATE TABLE user(pk_user_id INT PRIMARY KEY AUTOINCREMENT NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), e_mail VARCHAR(255), last_login DATE)',
-  history: 'CREATE TABLE history(pk_history_id INT PRIMARY KEY AUTOINCREMENT NOT NULL, FOREIGN KEY (fk_user_id) REFERENCES user(pk_user_id), id_input INT, name VARCHAR(255), e_mail VARCHAR(255), faculty VARCHAR(255), subject VARCHAR(255), server_group VARCHAR(255), gender VARCHAR(255))',
-  faculty: 'CREATE TABLE faculty(pk_faculty_id INT PRIMARY KEY , faculty_name VARCHAR(255), university_name VARCHAR(255))',
-  LDAP_ServerGroup: 'CREATE TABLE ldap(pk_server_group_id INT PRIMARY KEY AUTOINCREMENT NOT NULL, server_group_name VARCHAR(255) NOT NULL, people VARCHAR(255) NOT NULL, dc VARCHAR(255) NOT NULL, lang VARCHAR(255) NOT NULL, group_priority INT NOT NULL)',
-  VPN_ServerConfig: 'CREATE TABLE vpn_server_config(server_config_id INT PRIMARY KEY AUTOINCREMENT NOT NULL, FOREIGN KEY (fk_group_id) REFERENCES ldap(pk_server_group_id), config_file VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, port INTEGER NOT NULL, cert VARCHAR(255) NOT NULL , ca VARCHAR(255) NOT NULL )'
+  studySubject: 'CREATE TABLE study_subject(pk_study_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, study_name VARCHAR(255) NOT NULL, FOREIGN KEY (fK_faculty_name) REFERENCES faculty(pk_faculty_name))',
+  user: 'CREATE TABLE user(pk_user_id INTEGER PRIMARY KEY NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), e_mail VARCHAR(255))',
+  history: 'CREATE TABLE history(pk_history_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FOREIGN KEY (fk_user_id) REFERENCES user(pk_user_id), id_input INTEGER, name VARCHAR(255), e_mail VARCHAR(255), faculty VARCHAR(255), subject VARCHAR(255), server_group VARCHAR(255), gender VARCHAR(255))',
+  faculty: 'CREATE TABLE faculty(pk_faculty_id INTEGER PRIMARY KEY , faculty_name VARCHAR(255), university_name VARCHAR(255))',
+  LDAP_ServerGroup: 'CREATE TABLE ldap(pk_server_group_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, server_group_name VARCHAR(255) NOT NULL, people VARCHAR(255) NOT NULL, dc VARCHAR(255) NOT NULL, lang VARCHAR(255) NOT NULL, group_priority INTEGER NOT NULL)',
+  VPN_ServerConfig: 'CREATE TABLE vpn_server_config(server_config_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, FOREIGN KEY (fk_group_id) REFERENCES ldap(pk_server_group_id), config_file VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, port INTEGEREGER NOT NULL, cert VARCHAR(255) NOT NULL , ca VARCHAR(255) NOT NULL )'
 }
 
 /** @noinspection SqlResolve */
 const sqlConnection = require('sqlite3').verbose()
 
 //open database --> uses create/readwrite per default
-let db = new sqlConnection.Database('test.db', (err) => {
+let db = new sqlConnection.Database('./db/halt.db', (err) => {
   if (err) {
     console.error('Error connecting to database')
   }
@@ -35,11 +35,9 @@ function init_db () {
       .then(resolve => {
         createTable(createTableStatements.history)
       }).then(resolve => {
-      createTable(createTableStatements.LDAP_ServerGroup)
+      createTable(createTableStatements.faculty)
     }).then(resolve => {
       createTable(createTableStatements.studySubject)
-    }).then(resolve => {
-      createTable(createTableStatements.VPN_ServerConfig)
     }).catch(reject => {
       console.log('Error init DB')
       ok = false
@@ -62,15 +60,29 @@ function createTable (table) {
   })
 }
 
-function tablePresent (table_name) {
+function tablePresent (tableName) {
   console.log('table present called')
-  const statement = 'SELECT * FROM ' + table_name
+  const statement = 'SELECT * FROM ' + tableName
   let returnValue = true
   db.run(statement, (err) => {
     if (err) {
       returnValue = false
       console.log('Error')
     }
+  })
+  return returnValue
+}
+
+function userPresent (id) {
+  const statement = 'SELECT * FROM user WHERE pk_user_id == ' + id
+  let returnValue = false
+  const result = db.all(statement, (err, row) => {
+    if (err) {
+      console.log('Error in userPresent')
+      return
+    }
+    console.log(row)
+    returnValue = true
   })
   return returnValue
 }
@@ -104,12 +116,17 @@ function dropAll () {
   }))
 }
 
-function addUser (firstName, lastName, eMail) {
+function addUser (pk_user_id, firstName, lastName, eMail) {
   return new Promise(((resolve, reject) => {
     // noinspection SqlResolve
-    const statement = 'INSERT INTO user(first_name, last_name, e_mail, last_login) VALUES (? ,? ,?, ?)'
-    db.run(statement, [firstName, lastName, eMail], (err) => {
-
+    const statement = 'INSERT INTO user(pk_user_id, first_name, last_name, e_mail) VALUES (?,? ,? ,?)'
+    db.run(statement, [pk_user_id, firstName, lastName, eMail], (err) => {
+      if (err) {
+        console.log('Error adding new user: \n' + err)
+        reject()
+        return
+      }
+      resolve()
     })
   }))
 }
@@ -117,3 +134,14 @@ function addUser (firstName, lastName, eMail) {
 function deleteUser () {
 
 }
+
+createTable(createTableStatements.user).then(resolve => {
+  console.log('1')
+  return addUser(1, 'firstName', 'lastName', 'email')
+}, reject => {
+  console.log('2')
+  return addUser(1, 'firstName', 'lastName', 'email')
+}).then(resolve => {
+    console.log('cool')
+  }, reject => {console.log('error')}
+)
