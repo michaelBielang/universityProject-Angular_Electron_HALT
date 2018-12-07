ee6649bd899c57f37f006d71e12994311a894739
nrg <michael.bielang@hs-augsburg.de>
2018-12-07 10:15:47 +0100

    user controller

--- src/api/controlers/user.controler.ts
+++ src/api/controlers/user.controler.ts
@@ -4,12 +4,20 @@
  */
 
 import logger from '../logging/logger';
-
+import * as db from '../datastorage';
 
 export function user_show(req, res, next) {
-  logger.info(`User_show controler test`);
+  if (req.params['userid']) {
+    logger.info(`User_show controller test`);
+    db.dbInterface.dbFunctions.getUser()
+      res.status(200).json({
+      message: 'Retrieved user ' + req.params.userid + ' (API Test)',
+    });
+  } else {
+    res.status(409).json({
+      message: 'User not present',
+    });
+  }
 
-  res.status(200).json({
-    message: 'Retrieved user ' + req.params.userid + ' (API Test)',
-  });
+
 }
