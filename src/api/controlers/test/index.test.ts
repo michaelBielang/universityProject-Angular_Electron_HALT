428cb17ae2a31a28e207543df7420520be5b8751
chris <impulse@uni.de>
2018-12-21 10:05:31 +0100

    added blanko test files

---
+++ src/api/controlers/test/index.test.ts
@@ -0,0 +1,15 @@
+/**
+ * @author Michael Bielang, Christoph Bichlmeier
+ * @license UNLICENSED
+ *
+ * This test class should control all other controller tests
+ *
+ */
+
+
+class IndexTest {
+
+
+}
+
+export default new IndexTest();
